79a48186c7233288d331adf85d3f045d0fcf8d57
mohammedayaz24 <mohamedayaz2411@gmail.com>
2025-12-17 16:37:03 +0530

    control flow and more

--- controlflow.js
+++ controlflow.js
@@ -74,3 +74,47 @@ function getVal(val){
 }
 getVal(89);
 //it has a big issue used
+
+
+
+// practice question 
+
+function getGrade(score){
+    if(score>=90 && score<=100){
+        return 'A';
+    }
+    else if(score>=80 && score<90){
+        return 'B';
+    }
+    else if(score>=70 && score<80){
+        return 'C';
+    }
+    else if(score>=60 && score<70){
+        return 'D';
+    }
+    else if(score>=33 && score<60){
+        return 'E';
+    }
+    else if(score>=0 && score<33){
+        return 'Fail';
+    }
+    else{
+        return 'Invalid Score';
+    
+    }
+}
+
+console.log(getGrade(89));
+getGrade(1000);
+
+
+//Rock paper scissors logic
+
+function rps(user,computer){
+    if(user === computer) return 'draw';
+    if(user === 'rock' && computer === 'scissors') return 'user win';
+    if(user === 'scissors' && computer === 'paper') return 'user win';
+    if(user === 'paper' && computer === 'rock') return 'user win';
+    return 'computer win';
+}
+rps("rock","scissors");
