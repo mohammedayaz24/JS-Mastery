eae19866a52b4657ddbb9306e040b3f68b61b549
mohammedayaz24 <mohamedayaz2411@gmail.com>
2025-12-20 19:54:59 +0530

    functions basics

---
+++ functions.js
@@ -0,0 +1,27 @@
+//functions
+//function is a block of code designed to perform a particular task
+
+function dance(){
+    console.log("dance");
+    console.log("dance");
+    console.log("dance");
+}
+dance(); //function call
+
+//syntax
+/*function declaration
+function functionName(){
+    //code to be executed
+}
+functionName(); //function call*/
+
+//using variables 
+/* let fnc = function(){
+    //code
+}
+fnc(); call variable name because variable fnc contains function()*/
+
+let fnc = function(){
+    console.log("function stored in variable");
+}
+fnc(); //call variable name because variable fnc contains function()
