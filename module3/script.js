72a984383ca9b19d86375f7bf01be7c5810ffcd5
mohammedayaz24 <mohammedayaz2411@gmail.com>
2026-06-30 12:14:37 +0530

    module 3 started

---
+++ module3/script.js
@@ -0,0 +1,16 @@
+//scope
+//Which refers to how we can access the variable and function
+
+//functional scope,global scope and blocks scope
+
+//functional scope - can be used within the function Eg : var
+//Global Scope that can be used anywhere in the function : ed var
+//Block scope - can be accessed only within {} braces
+
+
+//Execution Content 
+//Execution Context is the environment where JavaScript executes your code.
+//  It stores variables, functions, and the value of this. 
+// It works in 2 phases: Memory Creation Phase (variables are initialized as undefined and functions are stored in memory) 
+// and Execution Phase (code runs line by line and variables get their actual values). 
+// JavaScript creates one Global Execution Context and a new Function Execution Context whenever a function is called.
