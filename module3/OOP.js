e35b5236863d1084edac06863c936c71068046ca
mohammedayaz24 <mohammedayaz2411@gmail.com>
2026-07-07 11:32:37 +0530

    callback and callback hell

--- module3/OOP.js
+++ module3/OOP.js
@@ -95,3 +95,76 @@ let u1 = new User('John Doe', '123 Main St', 'johndoe', 'john.doe@example.com', 
 let u2 = new User('Jaane', '456 Oak Ave', 'janedoe', 'jane.doe@example.com', 'User');
 let a1 = new Admin('Jane Smith', '456 Oak Ave', 'janesmith', 'jane.smith@example.com', 'Admin');
 //
+
+
+//classical inheritance 
+//create class and extend it to create a subclass. The subclass inherits properties and 
+// methods from the superclass, allowing for code reuse and organization.
+
+
+//prototype inheritance
+let coffee = {
+    color : "dark",
+    drink : function(){
+        console.log(`I am a ${this.color} coffee`);
+}
+};
+
+let arabianCoffee = Object.create(coffee);
+console.log(arabianCoffee); //object in prototype
+arabianCoffee.drink()
+
+
+//sync    async
+
+//the code which runs line by line is sync
+//the code which is ready is run and then run is aync
+
+// console.log(Math.floor(Math.random()*10)*1000);
+
+//callback
+//callback - if you send a function to another function in parameter then that parameterized function is calledas callback 
+function willprintlater(fnc){
+    setTimeout(fnc , Math.floor(Math.random()*20)*1000);
+            
+}
+willprintlater(function(){
+    console.log("callback function");
+});
+
+//callback hell - when we have multiple nested callbacks, it can lead to a situation known as "callback hell" or "pyramid of doom".
+
+function getprofile(username, cb){
+    console.log("fetching profile...");
+    setTimeout(() => {
+        cb({_id: 1, username: username});
+    }, 1000);
+}
+
+function getAllpost(id, cb){
+    console.log("fetching all post...");
+    setTimeout(() => {
+        cb({_id: id, posts:["hey","hello","hi"]});
+    }, 2000);
+    
+}
+
+function savedPost(id, cb){
+    console.log("fetching saved post ....");
+    setTimeout(()=>{
+        cb({_id: id, saved:[1,2,3,4,5]});
+    },3000);
+
+}
+
+//callback hell
+getprofile("Ayaz",function(data){
+    console.log(data);
+    getAllpost(data._id, function(posts){
+        console.log(posts);
+        savedPost(data._id, function(saved){
+            console.log(saved);
+        });
+    });    
+});
+
