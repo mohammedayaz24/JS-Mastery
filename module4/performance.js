5b04fbdc7f2a7b55afb8cc9f145b953ca10abbb9
mohammedayaz24 <mohammedayaz2411@gmail.com>
2026-07-08 16:11:49 +0530

    throttle and rebouncing

--- module4/performance.js
+++ module4/performance.js
@@ -1,4 +1,4 @@
-//Debouncing
+//Debouncing -> ek delay bataaoge tum utna delay jab bhi ayega action la reaction milega
 let input = document.querySelector("input");
 function debounce(fnc, delay){
     let timer;
@@ -11,11 +11,26 @@ function debounce(fnc, delay){
 }
 
 
-input.addEventListener("input",debounce(function(){
-    console.log("Hii");
+// input.addEventListener("input",debounce(function(){
+//     console.log("Hii");
+// },1000));
+
+
+
+
+//throttle_> interval per chalunga action hota raha and apne ek interval bataya utne interval me apka event chalega
+
+function throttle(fnc,delay){
+    let timer = 0;
+    return function(...args){
+        let now = Date.now();
+        if(now - timer >= delay){
+            timer = now;
+            fnc(...args);
+        }
+    };
+}
+
+input.addEventListener("input",throttle(function(){
+    console.log(`throttle`);
 },1000));
-
-
-
-
-//throttle
