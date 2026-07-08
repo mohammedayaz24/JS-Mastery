6ff08728275c7288fa1f2186463add8b6aefb6b5
mohammedayaz24 <mohammedayaz2411@gmail.com>
2026-07-08 15:19:20 +0530

    performance optimization

---
+++ module4/performance.js
@@ -0,0 +1,21 @@
+//Debouncing
+let input = document.querySelector("input");
+function debounce(fnc, delay){
+    let timer;
+    return function(...args){
+        clearTimeout(timer);
+        timer = setTimeout(() =>{
+            fnc(...args);
+        },delay);
+    }
+}
+
+
+input.addEventListener("input",debounce(function(){
+    console.log("Hii");
+},1000));
+
+
+
+
+//throttle
